467cb327ade605c3ca14b1e8680b16d1aac152ed
GiovanItsur <s24120131@alumnos.itsur.edu.mx>
2026-03-12 00:50:08 -0600

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/example/calculator/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/calculator/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import ui.TipCalculatorScreen
 
 class MainActivity : ComponentActivity() {
@@ -14,9 +12,4 @@ class MainActivity : ComponentActivity() {
             TipCalculatorScreen()
         }
     }
-}
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
